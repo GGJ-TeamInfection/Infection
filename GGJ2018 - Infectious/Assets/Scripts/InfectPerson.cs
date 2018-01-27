debcf55a07bdf779fb7a83e231c66d17346f8254
Randy Phillips <calibermengsk@gmail.com>
2018-01-27 11:00:52 -0600

    Basic player movement (wasd/arrows)

---
+++ GGJ2018 - Infectious/Assets/Scripts/InfectPerson.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectPerson : MonoBehaviour {
+
+    GameObject[] objectsInRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    
+    }
+}
---
+++ GGJ2018 - Infectious/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 3;
+
+    Vector2 moveDirection;
+
+    Rigidbody2D rb;
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = moveDirection * speed;
+	}
+}
