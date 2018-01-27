45c9e209b4a81e6b11ae9d143ddf0fe335e8a5fd
Peter Christodoulou <pjchristodoulou@yahoo.com>
2018-01-27 15:28:05 -0600

    Camera Follow Resistance - some players require repeat infection Borders

--- GGJ2018 - Infectious/Assets/Scripts/PersonAI.cs
+++ GGJ2018 - Infectious/Assets/Scripts/PersonAI.cs
@@ -9,6 +9,7 @@ public class PersonAI : MonoBehaviour {
         maxWaitTime = 10000;
     public Vector2 moveSize = new Vector2(3, 3);
     public bool infected = false;
+	public float resistance = 3f;
 
     public float curTime = 0,
         targetTime = 0;
@@ -58,8 +59,15 @@ public class PersonAI : MonoBehaviour {
     
     public void GetInfected()
     {
-        infected = true;
-        targetPosition = new Vector3(Random.Range(-moveSize.x, moveSize.x), Random.Range(-moveSize.y, moveSize.y), 0);
+		resistance = (resistance > 0f ? resistance - 1f : 0f);
+
+		infected = resistance == 0f;
+		if (infected) {
+			targetPosition = new Vector3 (Random.Range (-moveSize.x, moveSize.x), Random.Range (-moveSize.y, moveSize.y), 0);
+			GetComponent<SpriteRenderer> ().color = Color.red;
+		} else {
+			GetComponent<SpriteRenderer> ().color = Color.yellow;
+		}
     }
 
     public void StopInfection()
