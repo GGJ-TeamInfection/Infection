f03e1e5905feaa34462fb52ae6a14305bf42a0b6
Randy Phillips <calibermengsk@gmail.com>
2018-01-28 12:11:12 -0600

    Art added in! Added in animations and art to level Game loop complete! NO SOUND YET!

--- GGJ2018 - Infectious/Assets/Scripts/PlayerMovement.cs
+++ GGJ2018 - Infectious/Assets/Scripts/PlayerMovement.cs
@@ -10,16 +10,37 @@ public class PlayerMovement : MonoBehaviour {
     public static GameObject instance;
 
     Rigidbody2D rb;
+    Animator anim;
     // Use this for initialization
 	void Start () {
         instance = gameObject;
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(Mathf.Abs(moveDirection.x) > 0.1f || Mathf.Abs(moveDirection.y) > 0.1f)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if(moveDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        if (moveDirection.x > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
         rb.velocity = moveDirection * speed;
 	}
 }
